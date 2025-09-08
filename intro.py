02386ad0437e50465a4a980b69f591d823b1cf96
Julia Pockat <julia.pockat@gmail.com>
2025-09-08 10:26:55 -0400

    plotting the image

--- intro.py
+++ intro.py
@@ -1,3 +1,21 @@
-import astropy
-import photutils
+from astropy.io import fits
+import matplotlib.pyplot as plt
+from photutils import aperture
 
+hdul = fits.open('12410-PS-DATA copy/PSDataFile.001.fits')
+
+hdul.info()
+
+image_data = hdul[0].data
+
+plt.figure(figsize=(8, 8))
+plt.imshow(image_data)
+plt.title('PS Data File 001')
+
+plt.xlabel('Length of Data Axis 1')
+plt.ylabel('Length of Data Axis 2')
+
+plt.colorbar()
+plt.show()
+
+hdul.close()
